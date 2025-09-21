cae7bf3abc2637f875b361479ed19321ed76a6bb
123SHANIDENGRE <shanidengre@gmail.com>
2025-09-21 13:24:57 +0530

    userinfo-service

---
+++ src/main/java/com/codedecode/userInfo/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.codedecode.userInfo.repository;
+
+import com.codedecode.userInfo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+
+}
